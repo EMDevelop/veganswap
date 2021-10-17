c0be60d633ae57ebd36ff60a87f227e9f5a64be4
EMDevelop <edwardmartin.inbox@gmail.com>
2021-10-17 13:32:39 +0100

    ssl v2

--- db/index.js
+++ db/index.js
@@ -4,13 +4,14 @@ const devConfig = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@
 
 const proConfig = process.env.DATABASE_URL; //heroku addons
 
+const config = parse(process.env.DATABASE_URL);
+
 config.ssl = {
   rejectUnauthorized: false,
 };
 
 const pool = new Pool({
-  connectionString:
-    process.env.NODE_ENV === 'production' ? proConfig : devConfig,
+  connectionString: process.env.NODE_ENV === 'production' ? config : devConfig,
 });
 
 module.exports = {
