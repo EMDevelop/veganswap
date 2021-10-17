ab7120230e8d85073796565cf5e1bb5eda38afe1
EMDevelop <edwardmartin.inbox@gmail.com>
2021-10-17 20:09:21 +0100

    attempting hard coded axios base url

--- client/src/apis/axios.js
+++ client/src/apis/axios.js
@@ -5,10 +5,12 @@ import axios from 'axios';
 // we will then see that because the front end and back end are on the same machine now:
 // baseurl now = /api/v1/
 
-const baseURL =
-  process.env.NODE_ENV === 'production'
-    ? 'api/v1'
-    : 'http://localhost:3001/api/v1';
+// const baseURL =
+//   process.env.NODE_ENV === 'production'
+//     ? 'api/v1'
+//     : 'http://localhost:3001/api/v1';
+
+const baseURL = 'api/v1';
 
 export default axios.create({
   baseURL,
