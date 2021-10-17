0cefe4f1db6cac6134814c380f36138486a89a10
EMDevelop <edwardmartin.inbox@gmail.com>
2021-10-17 18:51:54 +0100

    rollback

--- db/index.js
+++ db/index.js
@@ -1,18 +1,19 @@
 const { Pool } = require('pg');
-const { parse } = require('pg-connection-string');
+// const { parse } = require('pg-connection-string');
 
 const devConfig = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`;
 
 const proConfig = process.env.DATABASE_URL; //heroku addons
 
-const config = parse(process.env.DATABASE_URL);
+// const config = parse(process.env.DATABASE_URL);
 
-config.ssl = {
-  rejectUnauthorized: false,
-};
+// config.ssl = {
+//   rejectUnauthorized: false,
+// };
 
 const pool = new Pool({
-  connectionString: process.env.NODE_ENV === 'production' ? config : devConfig,
+  connectionString:
+    process.env.NODE_ENV === 'production' ? proConfig : devConfig,
 });
 
 module.exports = {
