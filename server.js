9b26037d0140aad1072d0a5119c845e88ffd3c37
EMDevelop <edwardmartin.inbox@gmail.com>
2021-10-19 17:33:36 +0100

    rollback of CORS commenting out

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`server is up mate on port ${port}`);
 });
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 //
